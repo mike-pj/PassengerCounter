d40720fd43dd8d1d66ac6ec26546168c0220d72a
mike <michaeluzoigwe78@gmail.com>
2022-03-17 15:16:42 -0200

    previous enteries

--- index.js
+++ index.js
@@ -3,14 +3,17 @@
 let count = 0
 
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
 function increment() {
-    count = count + 1
+    count += 1
     countEl.innerText = count
-    console.log(count)
+
 }
 
 function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr  
     console.log(count)
 }
 
